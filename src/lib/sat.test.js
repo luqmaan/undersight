16983aaff330612cdbfac7d5e544fb37fbe2919c
Luqmaan <ldawoodjee@gmail.com>
2016-10-29 20:44:21 -0500

    wef

--- src/lib/sat.test.js
+++ src/lib/sat.test.js
@@ -7,6 +7,68 @@ import sortBy from 'lodash/sortBy';
 import counters from '../data/counters.json';
 import heros from '../data/heros.json';
 
+function getTeamPicksV2(teamPicks, competitive) {
+  const allHeros = heros.map((hero) => hero.name);
+
+  const teamHardCounters = uniq(flatten(teamPicks.map((heroName) => {
+    return counters
+      .filter((counter) => counter.you === heroName)
+      .filter((counter) => counter.score === 2)
+      .map((counter) => counter.enemy);
+  })));
+
+  const uncountersHeros = difference(allHeros, teamHardCounters);
+  const notPicked = difference(allHeros, competitive ? teamPicks : []);
+
+  const scores = notPicked.map((heroName) => {
+    const numHardCounters = counters.reduce((prev, counter) => {
+      if (counter.you === heroName) {
+        return prev + counter.score;
+      }
+      return prev;
+    }, 0);
+
+    return {
+      score: numHardCounters,
+      heroName,
+    };
+  });
+
+  return sortBy(scores, 'score').reverse();
+}
+
+
+function getTeamPicksV3(teamPicks, competitive) {
+  const allHeros = heros.map((hero) => hero.name);
+
+  const teamHardCounters = uniq(flatten(teamPicks.map((heroName) => {
+    return counters
+      .filter((counter) => counter.you === heroName)
+      .filter((counter) => counter.score === 2)
+      .map((counter) => counter.enemy);
+  })));
+
+  const uncountersHeros = difference(allHeros, teamHardCounters);
+  const notPicked = difference(allHeros, competitive ? teamPicks : []);
+
+  const scores = notPicked.map((heroName) => {
+    const numHardCounters = counters.reduce((prev, counter) => {
+      if (counter.you === heroName && uncountersHeros.indexOf(counter.enemy) !== -1) {
+        return prev + counter.score;
+      }
+      return prev;
+    }, 0);
+
+    return {
+      score: numHardCounters,
+      heroName,
+    };
+  });
+
+  return sortBy(scores, 'score').reverse();
+}
+
+
 function getTeamPicks(teamPicks, competitive) {
   const allHeros = heros.map((hero) => hero.name);
 
@@ -38,32 +100,14 @@ function getTeamPicks(teamPicks, competitive) {
 }
 
 
+
 describe('sat', () => {
   fit('should asdf', () => {
-    // T = your team
-    // A = heroes currently hard countered by your team picks
-    // find the hero not currently picked, who has the most hard counters who are not in a
-    //
-    // uncountersHeros = all heros - A
-    // notPicked = all heros - T
-    // hero with the most hard counters in uncountersHeros
-    //
-    // heros = []
-    //
-    // for hero in T:
-    //   hard_ctrs = 0
-    //   for ctr in counters:
-    //     if ctr.you == hero and ctr.score == -2 and ctr.enemy in uncountersHeros:
-    //       hard_ctrs += 1
-    //   heros.append((hero, hard_ctrs))
-    //
-    // heros = sorted(cmp=lambda a, b: a[1] - b[1])
-    //
-    // num_hard_ctrs = counters[(counters.you == hero) & (counters.score == -2) & (counters.enemy in uncountersHeros)]
-    // if num_hard_ctrs > max_hard_ctrs:
-    //   current_pick = hero
+    console.log(getTeamPicks(['Tracer', 'Junkrat', 'Hanzo', 'Mei', 'Bastion', 'Reinhardt'], false))
+    // console.log(getTeamPicksV2(teamPicks, false))
+    console.log(getTeamPicksV3(['Tracer', 'Junkrat', 'Pharah', 'McCree', 'Mei', 'Bastion'], false))
 
-    expect(getTeamPicks(['Pharah', 'D.Va', 'D.Va', 'D.Va', 'D.Va', 'D.Va'], false)).toMatchSnapshot();
+    // expect(getTeamPicks(['Pharah', 'D.Va', 'D.Va', 'D.Va', 'D.Va', 'D.Va'], false)).toMatchSnapshot();
 
   });
 
