05d4a2f068b77d2ef75a002cfd04b45e53c10a92
Luqmaan <ldawoodjee@gmail.com>
2016-11-21 13:06:23 -0500

    miss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Toolbar, NavItem} from 'rebass';
-import {HashRouter, Match, Miss, Link} from 'react-router'
+import {HashRouter, Match, Miss, Link, Redirect} from 'react-router'
 
 import Reference from './components/Reference';
 import Calculator from './components/Calculator';
@@ -18,7 +18,7 @@ export default class App extends Component {
           <Match pattern="/reference" component={Reference} />
           <Match pattern="/reference/:heroName" component={Reference} />
           <Match pattern="/calculator" component={Calculator} />
-          <Miss component={() => <Redirect to="/dashboard"/>} />
+          <Miss component={() => <Redirect to="/calculator" />} />
           <div className="Bottombar">
             <Link to="/calculator">{(props) => <BottombarLink label="Calculator" {...props} />}</Link>
             <Link to="/reference">{(props) => <BottombarLink label="Reference" {...props} />}</Link>
